e66e58e350ccf3a697796b3a5ff8c26457d5c609
oleh <olehmisar@gmail.com>
2025-07-25 16:01:56 +0000

    use vitest

---
+++ vitest.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    // aztec sandbox tests take quite some time
+    hookTimeout: 200000,
+    testTimeout: 200000,
+  },
+});
