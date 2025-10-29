9e68005e5df245fbd07dab78c20558eda88188de
zorzal <zorzal@defi.sucks>
2025-10-29 18:39:14 +0000

    chore: remove commented code

--- src/ts/utils.ts
+++ src/ts/utils.ts
@@ -5,21 +5,6 @@ import {
 } from "../artifacts/Counter.js";
 import { AztecAddress } from "@aztec/stdlib/aztec-address";
 import { Contract } from "@aztec/aztec.js/contracts";
-import { PXE } from "@aztec/pxe/server";
-
-// export const createPXE = async (id: number = 0) => {
-//   const { BASE_PXE_URL = `http://localhost` } = process.env;
-//   const url = `${BASE_PXE_URL}:${8080 + id}`;
-//   const pxe = await PXE.create(
-//     node, store,
-//   )
-//   // await waitForPXE(pxe);
-//   return pxe;
-// };
-
-// export const setupSandbox = async () => {
-//   return createPXE();
-// };
 
 /**
  * Deploys the Counter contract.
