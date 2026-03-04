b62825132c1f50bc50073902a1d9cd81d69b9fe4
ilpepepig <ilpepepig@defi.sucks>
2026-03-04 14:37:54 -0300

    chore: remove vitest.setup.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -16,7 +16,6 @@ export default defineConfig({
     // aztec sandbox tests take quite some time
     hookTimeout: 200000,
     testTimeout: 200000,
-    globalSetup: "./vitest.setup.ts",
     fileParallelism: false,
     pool: "forks",
     singleFork: true,
